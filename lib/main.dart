1f8f3575ce6ee5b5be80c3f387389fadb1e6291e
pengliheng <pengliheng111@gmail.com>
2018-12-31 21:32:28 +0800

    add picture

--- lib/main.dart
+++ lib/main.dart
@@ -18,66 +18,80 @@ class MyApp extends StatelessWidget {
 
 
 
-
 class ListContainer extends StatelessWidget {
-
+  // 相对间隔
+  double eachItemPaddingValue = 2.0;
   ListContainer({@required this.count});
-  final count;
-  // List<Container> _buildGridTileList(int count) {
-  //   return new List<Container>.generate(count, 
-  //     (int index) => new Container(
-  //       // width: 100,
-  //       child: new Row(
-  //         children: <Widget>[
-  //           imageContent(index),
-  //           imageContent(index+1),
-  //           imageContent(index+2),
-            
-  //         ],
-  //       )
-  //     )
-  //   );
-  // }
-
+  var count;
   @override
   Widget build(BuildContext context) {
-
-    double width = MediaQuery.of(context).size.width;
-    imageContent(int index) {
+    double vw = MediaQuery.of(context).size.width;
+    imageContent(int index, bool isBlock, [bool isAddBtn = false]) {
       return new Expanded(
         flex: 1,
         child: Container(
-          height: 150,
-          padding: EdgeInsets.all(2),
-          child: Image.asset(
+          height: ((vw - eachItemPaddingValue*2)/3),
+          padding: EdgeInsets.all(eachItemPaddingValue),
+          child: isBlock ? Image.asset(
             'assets/${index+1}.jpg',
             fit: BoxFit.cover,
-          )
+          ) : Container()
         )
       );
     }
 
-    _buildGridTileRow(int index) {
+    _buildGridTileRow(int cols, int total) {
+      List<Widget> ListMyWidgets(){
+        List<Widget> list = new List();
+        var colNum = total - (cols * 3);
+        if(colNum>3){
+          colNum = 3;
+        }
+        for (var i = 0; i < 3; i++) {
+          if(cols==0 && i==0){
+            list.add(new Expanded(
+              flex: 1,
+              child: Container(
+                height: ((vw - eachItemPaddingValue*2)/3),
+                padding: EdgeInsets.all(eachItemPaddingValue),
+                child: Container(
+                  color: Colors.white,
+                  child: Center(
+                    child: Container(
+                      child: Icons.add
+                    ),
+                  ),
+                )
+              )
+            ));
+          } else {
+            list.add(imageContent(3*cols+i,colNum>i));
+          }
+        }
+        return list;
+      }
       return new Row(
-        children: <Widget>[
-          imageContent(3 * index),
-          imageContent(3 * index + 1),
-          imageContent(3 * index + 2),
-        ],
+        children: ListMyWidgets(),
       );
     }
 
+    /* 
+     * require            count                     总数
+     * todo               loop                      传递参数 -> 
+     * througt            当前行 个数                 传递参数 -> 
+     * return             widget
+     */
     _buildGridTileList(int count) {
       final cols = 1 + count~/3;
-      return new List<Container>.generate(cols, 
-        (int index) => new Container(
-          // width: 100,
-          child: _buildGridTileRow(index)
+      return new List<Container>.generate(
+        cols, 
+        (int index) => new Container(         // 循环多少行
+          child: _buildGridTileRow(index , count)
         )
       );
     }
     return new Container(
-      padding: EdgeInsets.all(2),
+      padding: EdgeInsets.all(eachItemPaddingValue),
       color: Colors.red,
       child: ListView(
         children: _buildGridTileList(this.count)
