2db804fbdc59b3c2937eface2aa3b668b505c9a2
pengliheng <pengliheng111@gmail.com>
2019-01-03 01:46:18 +0800

    add image introduct

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ### Flutter 入门
 
 
+![image](https://tinypng.com/web/output/cf2eq1bc8nzpdy8mgudk25w3pvpn01m9/WechatIMG1.jpeg)
+
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -6,23 +6,93 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  // TODO: implement widget
+  Widget build(BuildContext context) {
+    return new Scaffold(
       appBar: new AppBar(
         title: new Center(
           child: Text('My First App'),
         ),
       ),
-      body: new Center(
-        child: new Text(
-          'Hello World',
-          style: TextStyle(
-            fontFamily: 'babas-neue',
-            fontSize: 24.0,
-            color: Colors.red
-          ),
-        ),
-      ),
+      body: _MyList()
     );
   }
 }
 
+
+
+class _MyList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new ListView.builder(
+      padding: EdgeInsets.all(4),
+      itemBuilder: (context, i){
+        return new ListTile(
+          title: new Text("Some Random Username"),
+          subtitle: Text(
+            'Online  ->  $i',
+            style: new TextStyle(
+              fontStyle: FontStyle.italic,
+              color: Colors.green[400]
+            ),
+          ),
+          leading: const Icon(Icons.face),
+          trailing: new RaisedButton(
+            child: new Text('Remove'),
+            onPressed: (){
+              deleteDialog(context).then((value){
+                print('Value is $value');
+              });
+              print('your press this button');
+            },
+          ),
+        );
+      },
+    );
+  }
+}
+
+
+
+Future<bool> deleteDialog(BuildContext context) {
+  return showDialog(
+    context: context,
+    // 点击其他界面触发隐藏弹出框
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      return new AlertDialog(
+        title: Text('are u sure?'),
+        actions: <Widget>[
+          new FlatButton(
+            child: new Text('Yes'),
+            onPressed: (){
+              Navigator.of(context).pop(true);
+            },
+          ),
+          new FlatButton(
+            child: new Text('No'),
+            onPressed: (){
+              Navigator.of(context).pop(false);
+            },
+          ),
+        ]
+      );
+    }
+  );
+}
