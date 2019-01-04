3227cbb918285573afa2606bc904e8e28c60569b
pengliheng <pengliheng111@gmail.com>
2019-01-04 20:53:38 +0800

    finish tab buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './firstPage.dart' as firstpage;
+import './secondPage.dart' as secondpage;
 
 void main() => runApp(MyApp());
 
@@ -6,7 +8,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MyHomePage();
+    return new MaterialApp(
+      theme: new ThemeData(
+        primarySwatch: Colors.teal
+      ),
+      home:  MyHomePage(),
+    );
   }
 }
 
@@ -18,20 +25,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 
-class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin{
-  TabController controller;
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  TabController tapController;
 
   @override
-    void initState() {
-      super.initState();
-      controller = new TabController(vsync: this,length: 2);
-    }
+  void initState() {
+    super.initState();
+    tapController = new TabController(vsync: this,length: 2);
+  }
 
   @override
-    void dispose() {
-      controller.dispose();
-      super.dispose();
-    }
+  void dispose() {
+    tapController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,24 +46,31 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
       appBar: new AppBar(
         title: Text('Tap App'),
         backgroundColor: Colors.teal,
-        bottom: new TabBar(
-          controller: controller,
+        // bottom: new TabBar(
+        //   controller: tapController,
+        //   tabs:<Widget>[
+        //     new Tab(icon: Icon(Icons.access_alarm)),
+        //     new Tab(icon: Icon(Icons.account_balance)),
+        //   ]
+        // )
+      ),
+      bottomNavigationBar: new Material(
+        color: Colors.teal,
+        child: new TabBar(
+          controller: tapController,
           tabs:<Widget>[
             new Tab(icon: Icon(Icons.access_alarm)),
-            new Tab(icon: Icon(Icons.access_time)),
-          ]
+            new Tab(icon: Icon(Icons.account_balance)),
+          ],
         )
       ),
-      // body: new Center(
-      //   child: new Text(
-      //     'Hello World',
-      //     style: TextStyle(
-      //       fontFamily: 'babas-neue',
-      //       fontSize: 24.0,
-      //       color: Colors.red
-      //     ),
-      //   ),
-      // ),
+      body: new TabBarView(
+        controller: tapController,
+        children: <Widget>[
+          new firstpage.FirstPage(),
+          new secondpage.SecondPage(),
+        ],
+      )
     );
   }
 }
