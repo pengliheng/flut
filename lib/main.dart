799b059006e3ce6e8e35d575da39ed0559544371
pengliheng <pengliheng111@gmail.com>
2019-01-06 16:22:38 +0800

    navigator with two page

--- lib/main.dart
+++ lib/main.dart
@@ -22,55 +22,21 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage>{
   final textCtrl = new TextEditingController();
-  String inputstr;
-  String radioval;
-  bool enabled = false;
-  bool expanded = false;
-  bool checked = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: new Text('SideMenu'),
       ),
-      drawer: new Drawer(
-        child: ListView(
-          children: <Widget>[
-            new UserAccountsDrawerHeader(
-              accountName: new Text("Raja"),
-              accountEmail: new Text("pengliheng111@gmail.com"),
-              currentAccountPicture: new CircleAvatar(
-                backgroundImage: new NetworkImage('http://i.pravatar.cc/300'),
-              ),
+      body: new CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 250.0,
+            flexibleSpace: new FlexibleSpaceBar(
+              background: Image.network('src'),
             ),
-            new ListTile(
-              title: Text('About Page'),
-              onTap: () {
-                Navigator.of(context).pop();
-                Navigator.push(context, 
-                  new MaterialPageRoute(
-                    builder: (BuildContext context) => new AboutPage()
-                  )
-                );
-              }
-            ),
-            new Divider(
-              color: Colors.black,
-              height: 5.0,
-            ),
-            new ListTile(
-              title: Text('About Page'),
-              onTap: () {
-                Navigator.of(context).pop();
-                Navigator.push(context, 
-                  new MaterialPageRoute(
-                    builder: (BuildContext context) => new AboutPage()
-                  )
-                );
-              }
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
