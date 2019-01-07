62b13311be34668497706e0b6caa31edd1063ec2
pengliheng <pengliheng111@gmail.com>
2019-01-08 00:10:06 +0800

    collect code

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_map/flutt er_map.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
+// import 'package:geolocation/geolocation.dart';
 
 void main() => runApp(MyApp());
-
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -25,10 +26,38 @@ class _MyHomePageState extends State<MyHomePage>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: new Text('Left Maps'),
+        title: new Text('Leaflet Maps'),
       ),
-      body: Container(
-        child: Text('data')
+      body: FlutterMap(
+        options: new MapOptions(
+          center: new LatLng(40.71, -74.20),
+          minZoom: 10.0,
+        ),
+        layers: [
+          new TileLayerOptions(
+            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            subdomains: ['a','b','c'],
+          ),
+          new MarkerLayerOptions(
+            markers: [
+              new Marker(
+                width: 45.0,
+                height: 45.0,
+                point: new LatLng(40.71, -74.10),
+                builder: (context) => new Container(
+                  child: IconButton(
+                    icon: Icon(Icons.location_on),
+                    color: Colors.blue,
+                    iconSize: 45.0,
+                    onPressed: (){
+                      print('Marker taped');
+                    },
+                  ),
+                )
+              )
+            ]
+          )
+        ],
       )
     );
   }
